234dd1abcb276bbfc6ed7b89b86b09f73633e826
Evelina <evelinazolota55@gmail.com>
2025-07-27 20:49:31 +0300

    обнова 2

--- src/ru/yandex/javacourse/service/TaskManager.java
+++ src/ru/yandex/javacourse/service/TaskManager.java
@@ -7,24 +7,39 @@ import java.util.List;
 public interface TaskManager {
 
     void addTask(Task task);
+
     Task getTask(int id);
+
     List<Task> getAllTasks();
+
     void updateTask(Task task);
+
     void removeTaskById(int id);
+
     void removeAllTasks();
 
     void addEpic(Epic epic);
+
     Epic getEpic(int id);
+
     List<Epic> getAllEpics();
+
     void updateEpic(Epic epic);
+
     void removeEpicById(int id);
+
     void removeAllEpics();
 
     void addSubtask(Subtask subtask);
+
     Subtask getSubtask(int id);
+
     List<Subtask> getAllSubtasks();
+
     void updateSubtask(Subtask subtask);
+
     void removeSubtaskById(int id);
+
     void removeAllSubtasks();
 
     List<Subtask> getSubtasksOfEpic(int epicId);
