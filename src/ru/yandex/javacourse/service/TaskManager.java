f65471799f1832497ebb2e20b567c027ba012b72
Evelina <evelinazolota55@gmail.com>
2025-10-17 15:29:39 +0300

    спринт 8: обнова 1

--- src/ru/yandex/javacourse/service/TaskManager.java
+++ src/ru/yandex/javacourse/service/TaskManager.java
@@ -7,29 +7,42 @@ import java.util.List;
 public interface TaskManager {
 
     List<Task> getTasks();
+
     List<Epic> getEpics();
+
     List<Subtask> getSubtasks();
 
     Task getTaskById(int id);
+
     Epic getEpicById(int id);
+
     Subtask getSubtaskById(int id);
 
     Task createTask(Task task);
+
     Epic createEpic(Epic epic);
+
     Subtask createSubtask(Subtask subtask);
 
     void updateTask(Task task);
-    void updateEpic(Epic epic);   // ⚡️ обязательно public
+
+    void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
 
     void deleteTasks();
+
     void deleteEpics();
+
     void deleteSubtasks();
 
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
+
     void deleteSubtaskById(int id);
 
     List<Subtask> getSubtasksOfEpic(int epicId);
+
     List<Task> getHistory();
 }
